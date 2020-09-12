2e1a246e1b7a22613fdb3a93277d0aeb4e59d03e
Mohammad Navabi <7navabi@gmail.com>
2020-09-12 14:32:36 +0430

    Update typography.tsx

--- src/styles/typography.tsx
+++ src/styles/typography.tsx
@@ -1,8 +1,9 @@
+import { Platform } from 'react-native';
 import { scaleFont } from './mixins';
 
 // FONT FAMILY
-export const FONT_FAMILY_REGULAR = 'OpenSans-Regular';
-export const FONT_FAMILY_BOLD = 'OpenSans-Bold';
+export const FONT_FAMILY_REGULAR = Platform.OS === 'ios' ? 'System' : 'OpenSans-Regular';
+export const FONT_FAMILY_BOLD = Platform.OS === 'ios' ? 'System' : 'OpenSans-Regular';
 
 // FONT WEIGHT
 export const FONT_WEIGHT_REGULAR = '400';
